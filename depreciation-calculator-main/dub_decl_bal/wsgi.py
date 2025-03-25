9a8bc8e7bbcab412c911ea14f835420bc228bf25
Steve M Silva <silvasm516@aol.com>
2025-03-24 22:55:22 -0700

    Update wsgi.py

--- depreciation-calculator-main/dub_decl_bal/wsgi.py
+++ depreciation-calculator-main/dub_decl_bal/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dub_decl_bal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'depreciation-calculator-main/dub_decl_bal.settings')
 
 application = get_wsgi_application()
